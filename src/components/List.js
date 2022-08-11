5bcc38cace073de61466982b745c59f4b8353990
blockodyssey-cheonga <cheonga@blockodyssey.io>
2022-08-11 15:04:18 +0900

    TailWindCss를 이용한 스타일링

--- src/components/List.js
+++ src/components/List.js
@@ -1,21 +1,6 @@
 import React from "react";
 
-const btnStyle = {
-  color: "#fff",
-  border: "none",
-  padding: "5px 9px",
-  borderRadius: "50%",
-  cursor: "pointer",
-  float: "right",
-};
-
 const List = ({ todo, setTodo }) => {
-  const getStyle = (completed) => ({
-    padding: "10px",
-    borderBottom: "1px dotted #ccc",
-    textDecoration: completed ? "line-through" : "none",
-  });
-
   const handleCompletedChange = (id) => {
     setTodo((prev) =>
       prev.map((todo) => {
@@ -37,16 +22,27 @@ const List = ({ todo, setTodo }) => {
   return (
     <div>
       {todo.map((todo) => (
-        <div style={getStyle(todo.completed)} key={todo.id}>
-          <input
-            type="checkBox"
-            defaultChecked={todo.completed}
-            onChange={() => handleCompletedChange(todo.id)}
-          />
-          {todo.title}
-          <button style={btnStyle} onClick={() => handleClick(todo.id)}>
-            x
-          </button>
+        <div key={todo.id}>
+          <div className="flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded">
+            <div className="items-center">
+              <input
+                type="checkBox"
+                defaultChecked={todo.completed}
+                onChange={() => handleCompletedChange(todo.id)}
+              />
+              <span className={todo.completed ? "line-through" : undefined}>
+                {todo.title}
+              </span>
+            </div>
+            <div className="items-center">
+              <button
+                className="px-4 py-2 float-right"
+                onClick={() => handleClick(todo.id)}
+              >
+                x
+              </button>
+            </div>
+          </div>
         </div>
       ))}
     </div>
